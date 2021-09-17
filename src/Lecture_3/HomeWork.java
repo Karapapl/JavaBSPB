5cd5cbb4971afa5126d07d03c4f9ac287a8e6097
Karapapl <44815446+Karapapl@users.noreply.github.com>
2021-09-17 11:43:51 +0300

    Fix Lecture_3\HomeWork

--- src/Lecture_3/HomeWork.java
+++ src/Lecture_3/HomeWork.java
@@ -3,34 +3,48 @@ package Lecture_3;
 public class HomeWork {
   public static void main(String[] args) {
 
-    String oneWord = "пенсионерка";
-    String twoWord = "покраснение";
-    char[] temp1 = new char[oneWord.length()];
-    char[] temp2 = new char[twoWord.length()];
-    char[] temp3 = new char[oneWord.length()];
+    String oneWord = "пЕнСи Онер к А";
+    String twoWord = "п  окра  С нение";
 
-    for (int j = 0; j < oneWord.length(); j++) {
-      temp1[j] = oneWord.charAt(j);
-      temp2[j] = twoWord.charAt(j);
-    }
+    oneWord = oneWord.toLowerCase().replace(" ", "");
+    twoWord = twoWord.toLowerCase().replace(" ", "");
+    if (oneWord.length() == twoWord.length()) {
+      char[] temp1 = oneWord.toCharArray();
+      char[] temp2 = twoWord.toCharArray();
+      char[] temp3 = new char[oneWord.length()];
+      char[] temp4 = new char[oneWord.length()];
 
-    for (int k = 0; k < oneWord.length(); k++) {
-      for (int j = 0; j < oneWord.length(); j++) {
-        if (temp1[k] == temp2[j]) {
-          temp3[k] = temp1[k];
-          break;
+      for (int k = 0; k < oneWord.length(); k++) {
+        for (int j = 0; j < oneWord.length(); j++) {
+          if (temp1[k] == temp2[j]) {
+            temp3[k] = temp1[k];
+            temp2[j] = 0;
+            break;
+          }
         }
       }
-    }
+      temp2 = twoWord.toCharArray();
+      for (int k = 0; k < oneWord.length(); k++) {
+        for (int j = 0; j < oneWord.length(); j++) {
+          if (temp2[k] == temp1[j]) {
+            temp4[k] = temp2[k];
+            break;
+          }
+        }
+      }
 
-    StringBuilder threeWord = new StringBuilder(String.valueOf(temp3));
+      StringBuilder threeWord = new StringBuilder(String.valueOf(temp3));
+      StringBuilder foreWord = new StringBuilder(String.valueOf(temp4));
 
-    if (oneWord.equals(threeWord.toString())) {
-      System.out.println("Слова " + "\"" + oneWord + "\"" + " и \"" + twoWord + "\" являются анаграммой.");
+      if (oneWord.equals(threeWord.toString()) && twoWord.equals(foreWord.toString())) {
+        System.out.println("Слова " + "\"" + oneWord + "\"" + " и \"" + twoWord + "\" являются анаграммой.");
+      } else {
+        System.out.println("Слова " + "\"" + oneWord + "\"" + " и \"" + twoWord + "\" не являются анаграммой.");
+      }
+
     } else {
-      System.out.println("Слова " + "\"" + oneWord + "\"" + " и \"" + twoWord + "\" не являются анаграммой.");
+      System.out.println("Длина слов \"" + oneWord + "\" и \"" + twoWord + "\" не совпадает, и слова не являются анаграммой.");
     }
-
   }
 
 }
