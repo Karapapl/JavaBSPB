bf97b11ebed8a6f36b6fdeb2082c4ed6a6ea48d4
Karapapl <44815446+Karapapl@users.noreply.github.com>
2021-10-26 10:31:32 +0300

    Added HomeWork for Lecture_5 part 1

---
+++ src/lecture_5_1/Main.java
@@ -0,0 +1,36 @@
+package lecture_5_1;
+
+public class Main {
+  public static void main(String[] args) {
+    ObjectsArray arrays = new ObjectsArray();
+    System.out.println("Размер массива равен " + arrays.size());
+    arrays.add((int) 1);
+    arrays.add((String) "1");
+    arrays.add((String) "банан");
+    arrays.add((int) 2);
+    System.out.println("Размер массива равен " + arrays.size());
+    arrays.add((String) "3");
+    arrays.add((String) "гранат");
+    arrays.add((String) "9");
+    arrays.add((String) "офис");
+    arrays.add((int) 3);
+    arrays.add((String) "5");
+    System.out.println("Размер массива равен " + arrays.size());
+    arrays.add((String) "девять");
+    System.out.println("Размер массива до удаления равен " + arrays.size());
+    System.out.println("Значение массови под индексом 5 до удаления равно " + arrays.get(5));
+    arrays.remove(5);
+    System.out.println("Размер массива после удаления равен " + arrays.size());
+    System.out.println("Значение массива под индексом 5 после удаления равно " + arrays.get(5));
+    arrays.add((String) "ппп");
+    arrays.add((String) "ааа");
+    arrays.add((String) "рпопго");
+    arrays.add((String) "впкеп");
+    arrays.add((String) "аппаапап");
+
+    System.out.println("Размер массива равен " + arrays.size());
+    System.out.println("Значение массива равно " + arrays.get(5));
+
+
+  }
+}
---
+++ src/lecture_5_1/ObjectsArray.java
@@ -0,0 +1,87 @@
+package lecture_5_1;
+
+public class ObjectsArray<T> {
+
+  private Object[] array = new Object[10];
+  private int size;
+
+  public int size() {
+    return size;
+  }
+
+  public void add(Object str) {
+    size++;
+    boolean approve = true;
+    boolean oldArray = true;
+    if (size > 1) {
+      for (int i = 0; i < size - 1; i++) {
+        if (array[i] == null) {
+          array[i] = str;
+          oldArray = false;
+        } else {
+
+          if (array[i].getClass() == str.getClass()) {
+            if (array[i].equals(str)) {
+              approve = false;
+              throw new ArrayStoreException("Значение " + str + " уже есть в данном массиве, его index равен " + i);
+            } else {
+              approve = true;
+            }
+
+          } else {
+            approve = true;
+          }
+        }
+      }
+    }
+
+    if (approve) {
+      if (array.length < size) {
+        Object[] newArray = new Object[array.length + 5];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        array = newArray;
+      }
+      if (oldArray) {
+        array[size - 1] = str;
+      }
+    }
+  }
+
+  public void put(Object str, int index) {
+    if (index > size - 1) {
+      throw new IndexOutOfBoundsException("Index " + index + "не может быть меньше " + (size - 1));
+    }
+    if (index < 0) {
+      throw new IndexOutOfBoundsException("Index не может быть меньше нуля");
+    }
+    array[index] = str;
+  }
+
+  public T get(int index) {
+    if (index > size - 1) {
+      throw new IndexOutOfBoundsException("Index " + index + "не может быть меньше " + (size - 1));
+    }
+    if (index < 0) {
+      throw new IndexOutOfBoundsException("Index не может быть меньше нуля");
+    }
+    return (T) array[index];
+  }
+
+  public void remove(int index) {
+    if (index > size - 1) {
+      throw new IndexOutOfBoundsException("Index " + index + "не может быть меньше " + (size - 1));
+    }
+    if (index < 0) {
+      throw new IndexOutOfBoundsException("Index не может быть меньше нуля");
+    }
+
+    Object[] newArray = new Object[array.length];
+    if (index > 0) {
+      System.arraycopy(array, 0, newArray, 0, (index - 1));
+
+    }
+    System.arraycopy(array, (index + 1), newArray, index, (array.length - index - 1));
+
+    array = newArray;
+  }
+}
